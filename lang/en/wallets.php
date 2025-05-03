7e33b9ae5a017bdd23442f1ed441a7d4bc87c5fe
Farshid Boroomand <farshid@luckytrip.co.uk>
2025-05-03 22:34:54 +0330

    language updated

--- lang/en/wallets.php
+++ lang/en/wallets.php
@@ -4,9 +4,15 @@ return [
     'not_found'            => 'Wallet not found',
     'invalid_wallet_id'    => 'Wallet id is not valid',
     'create_wallet_failed' => 'Wallet cannot be created',
+    'update_wallet_failed' => 'Wallet cannot be updated',
     'get_wallet_failed'    => 'Cannot get user wallet',
+    'insufficient_balance' => 'Insufficient balance',
     'wallet_transaction'   => [
         'increase_failed' => 'Wallet increase failed',
         'initial_amount'  => 'Wallet initial balance',
+    ],
+    'withdrawals' => [
+        'cannot_be_created'          => 'Withdrawal request cannot be created',
+        'sheba_numbers_are_the_same' => 'Sheba numbers cannot be the same'
     ]
 ];
