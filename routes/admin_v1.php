dbdfb1aa33e3fa1f34e7487f8de923f3db3fe014
Farshid Boroomand <farshid@luckytrip.co.uk>
2025-05-04 10:06:51 +0330

    route names are added

--- routes/admin_v1.php
+++ routes/admin_v1.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Modules\V1\Wallets\Controllers\Admin\AdminWithdrawalController;
 
 Route::prefix('admin/v1')->group(function () {
-    Route::get('/withdrawals', [AdminWithdrawalController::class, 'getAllRequestedWithdrawals']);
-    Route::post('/withdrawals/{withdrawal_id}', [AdminWithdrawalController::class, 'changeRequestedWithdrawalStatus']);
+    Route::get('/withdrawals', [AdminWithdrawalController::class, 'getAllRequestedWithdrawals'])->name('admin.withdrawals.get_list');
+    Route::post('/withdrawals/{withdrawal_id}', [AdminWithdrawalController::class, 'changeRequestedWithdrawalStatus'])->name('admin.withdrawals.update_status');
 });
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -8,17 +8,17 @@ use Modules\V1\Wallets\Controllers\API\WithdrawalController;
 
 Route::prefix('v1')->group(function () {
     Route::prefix('users')->group(function () {
-        Route::get('/', [UserController::class, 'userList']);
-        Route::post('/', [UserController::class, 'createUser']);
+        Route::get('/', [UserController::class, 'userList'])->name('user.list');
+        Route::post('/', [UserController::class, 'createUser'])->name('user.create');
 
-        Route::patch('/{user_id}/profiles/{profile_id}', [UserProfileController::class, 'updateUserProfile']);
+        Route::patch('/{user_id}/profiles/{profile_id}', [UserProfileController::class, 'updateUserProfile'])->name('user_profile.update');
     });
 
     Route::prefix('wallets')->group(function () {
-        Route::get('/{wallet_id}', [WalletController::class, 'getUserWallet']);
+        Route::get('/{wallet_id}', [WalletController::class, 'getUserWallet'])->name('wallet.get');
 
         Route::prefix('{wallet_id}/withdrawals')->group(function () {
-            Route::post('/sheba', [WithdrawalController::class, 'createWithdrawalRequest']);
+            Route::post('/sheba', [WithdrawalController::class, 'createWithdrawalRequest'])->name('withdrawal.create');
         });
     });
 });
